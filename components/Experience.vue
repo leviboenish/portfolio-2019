56ebedb73869eaeca6b87ed0de8f4afca9f636e9
Levi Boenish <leviboenish@gmail.com>
2021-03-31 14:08:14 -0600

    2021 update

--- components/Experience.vue
+++ components/Experience.vue
@@ -4,11 +4,20 @@
       <h2 class="section-header"><span class="exp-desktop">Experience</span> <span class="exp-mobile">XP</span></h2>
       <div class="job-history">
           <div class="job-info">
-               <h2 class="company">SomaLogic</h2>
-               <h3 class="dates">Jul 2019 &mdash; Present</h3>
+               <h2 class="company">A2Z Sync</h2>
+               <h3 class="dates">Jul 2021 &mdash; Present</h3>
           </div>
           <div class="job-description">
-            <p>At SomaLogic I am currently a UX/UI engineer. I am the project owner for how our consumer facing reports are rendered. I primarily work with HTML, CSS, and JS in Vue and we write our unit tests in Jest. I do everything from helping to design our apps in Figma to implementing how they are built.</p>
+            <p>I am currently a Full Stack Engineer at A2Z sync. I am specializing in react to help build a new app to propel the way we buy cars into the future. Within this app I am writing everything from styles to business logic. We are also using Cypress and PHP to test and develop back end code for our apps.</p>
+          </div>
+      </div>
+      <div class="job-history">
+          <div class="job-info">
+               <h2 class="company">SomaLogic</h2>
+               <h3 class="dates">Jul 2019 &mdash; Jan 2021</h3>
+          </div>
+          <div class="job-description">
+            <p>At SomaLogic I was a UX/UI engineer. I was the project owner for how our consumer facing reports are rendered. I primarily worked with HTML, CSS, and JS in Vue and we wrote our unit tests in Jest. I did everything from helping to design our apps in Figma to implementing how they were built.</p>
           </div>
       </div>
       <div class="job-history">
