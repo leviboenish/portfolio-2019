20ae8a98a9c99019f24dc9e243b01a1100a5694a
Levi Boenish <leviboenish@gmail.com>
2021-03-31 15:29:05 -0600

    about update

--- components/About.vue
+++ components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="section-container" id="about">
     <h3 class="sub-title">Front-End Developer</h3>
-    <p class="about-content">I’m Levi Boenish, a full stack developer with a specialization in front end technologies from Denver Colorado. I’ve been writing code for the past four years and am currently working on developing user interfaces for the medical space. Outside of writing code I am an avid climber, musician, and dungeon master currently running Curse of Strahd.</p>
+    <p class="about-content">I’m Levi Boenish, a full stack developer with a specialization in front end technologies from Denver Colorado. I’ve been writing code for the past five years and am currently working on developing user interfaces for the automotive industry. Outside of writing code I am an avid climber, musician, and dungeon master currently running Curse of Strahd.</p>
   </div>
 </template>
 
